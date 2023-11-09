c81ef11512d9d41fe127f3c05929ad39579c1134
Alexander Levin <alevin.ru@gmail.com>
2023-11-09 13:24:18 +0200

    index

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as getOne } from './getOne'
+export { default as getMany } from './getMany'
+export { default as postAny } from './postAny'
+export { default as defaultRoutes } from './defaultRoutes'
